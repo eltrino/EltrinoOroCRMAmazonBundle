0323eaca31f33839c2b1975e6d532ddd20eab433
Kostyantyn Stupak <kosmass.zt@gmail.com>
2015-08-07 15:43:47 +0300

    ABFOC-16: Migration for correct order data storing

---
+++ Migrations/Schema/v1_1/EltrinoOroCrmAmazonBundle.php
@@ -0,0 +1,51 @@
+<?php
+/*
+ * Copyright (c) 2014 Eltrino LLC (http://eltrino.com)
+ *
+ * Licensed under the Open Software License (OSL 3.0).
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://opensource.org/licenses/osl-3.0.php
+ *
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@eltrino.com so we can send you a copy immediately.
+ */
+
+namespace Eltrino\OroCrmAmazonBundle\Migrations\Schema\v1_1;
+
+use Doctrine\DBAL\Schema\Schema;
+use Oro\Bundle\MigrationBundle\Migration\Migration;
+use Oro\Bundle\MigrationBundle\Migration\QueryBag;
+
+class EltrinoOroCrmAmazonBundle implements Migration
+{
+    const TABLE_NAME = 'eltrino_amazon_order';
+
+    /**
+     * @param Schema $schema
+     * @param QueryBag $queries
+     */
+    public function up(Schema $schema, QueryBag $queries)
+    {
+        if ($schema->hasTable(self::TABLE_NAME)) {
+            $this->alterEltrinoAmazonOrderTable($schema);
+        }
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function alterEltrinoAmazonOrderTable(Schema $schema)
+    {
+        $table = $schema->getTable(self::TABLE_NAME);
+
+        if ($table->hasColumn('total_amount')) {
+            $table->changeColumn('total_amount', array(
+                'type'      => 'money',
+                'default'   => 0
+            ));
+        }
+    }
+} 
